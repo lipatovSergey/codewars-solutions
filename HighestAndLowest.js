6ee2b2c70071eac57b1375564ce871a82cd5917d
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-07-24 00:01:10 +0300

    Highest and Lowest

--- HighestAndLowest.js
+++ HighestAndLowest.js
@@ -1,3 +1,5 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
 function highAndLow(numbers) {
 	const splited = numbers.split(" ");
 	return `${Math.max(...splited)} ${Math.min(...splited)}`;
