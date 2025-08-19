74589cb718fb39228c2b291aa98faf64ed8abba4
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-08-19 03:30:11 +0300

    palindrome test

---
+++ isPalindrome.js
@@ -0,0 +1,22 @@
+// Check polindrome ignoring case and spaces
+function isPolindrome(s) {
+	const regex = /^[\p{L}]+$/u;
+	let arr = [];
+	let polindrome = true;
+	for (symbol of s.toLowerCase()) {
+		if (regex.test(symbol)) arr.push(symbol);
+	}
+
+	let left = 0;
+	let right = arr.length - 1;
+	while (left < right) {
+		if (arr[left] !== arr[right]) polindrome = false;
+		left++;
+		right--;
+	}
+	return polindrome;
+}
+
+console.log(isPolindrome("А роза упала на лапу Азора"));
+console.log(isPolindrome("No lemon, no melon"));
+console.log(isPolindrome("No lemon, no melop"));
